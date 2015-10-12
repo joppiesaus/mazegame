99892fc27632b1469249c78c1958dedf4342a57d
joppiesaus <job@function1.nl>
2015-10-12 19:34:02 +0200

    Big chunky pixels!

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@ var g;
 
 var camera, scene, renderer, timer;
 
+var ScreenWidth = 320, ScreenHeight = 200;
+
 var init = function()
 {
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerWidth, 0.1, 10000 );
     scene.add( camera );
 
     timer = new THREE.Clock();
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( ScreenWidth, ScreenHeight );
     document.body.appendChild(renderer.domElement);
 
     window.addEventListener( "resize", onWindowResize, false );
@@ -22,8 +24,6 @@ var onWindowResize = function()
 {
     camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
 };
 
 var start = function()
